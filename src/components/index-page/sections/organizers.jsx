1aa51504d8ad8a88a3e6ef62cc057649f7903aac
Mohammed FAKHAM <mohammed.fakham@gmail.com>
2019-08-29 22:59:15 +0100

    Add Organizers section content

--- src/components/index-page/sections/organizers.jsx
+++ src/components/index-page/sections/organizers.jsx
@@ -9,6 +9,17 @@ const Organizers = () => {
       <div className="about-content">
         <p className="small-text">GDG & WTM Casablanca</p>
         <h1>Organizers</h1>
+        <p className="p">
+          DevFest Casablaca is brought to you with love, efforts and endless fun
+          from the GDG & WTM Casablanca team.
+        </p>
+        <a
+          className="organizers-link"
+          href="https://www.meetup.com/GDGCasablanca/members/?op=leaders"
+          target="_blank"
+        >
+          Full organizers list
+        </a>
       </div>
     </Section>
   )
