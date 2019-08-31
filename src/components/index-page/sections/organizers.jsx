58be063b73310f3c3646208f10c525528003f039
Mohammed FAKHAM <mohammed.fakham@gmail.com>
2019-08-31 17:20:34 +0100

    Change class name to organizers

--- src/components/index-page/sections/organizers.jsx
+++ src/components/index-page/sections/organizers.jsx
@@ -5,7 +5,7 @@ import "./organizers.css"
 
 const Organizers = () => {
   return (
-    <Section classes="about" id="DevFest">
+    <Section classes="organizers" id="Organizers">
       <div className="about-content">
         <p className="small-text">GDG & WTM Casablanca</p>
         <h1>Organizers</h1>
