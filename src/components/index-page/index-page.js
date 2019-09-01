dd65cf064b024c8912720715e934721be6ffaf90
Omar Houmz <omar.houmz@adevinta.com>
2019-09-01 21:30:51 +0100

    feat: add sponsors section

--- src/components/index-page/index-page.js
+++ src/components/index-page/index-page.js
@@ -2,12 +2,14 @@ import React from "react"
 
 import About from "./sections/about"
 import Organizers from "./sections/organizers"
+import Sponsors from "./sections/sponsors"
 
 const IndexPage = () => {
   return (
     <div>
       <About />
       <Organizers />
+      <Sponsors />
     </div>
   )
 }
