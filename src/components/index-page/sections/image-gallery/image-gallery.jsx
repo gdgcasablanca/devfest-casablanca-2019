bfc543a3139dcd7b99d9341bc30167244aa5698e
Omar Houmz <omar.houmz@adevinta.com>
2019-11-06 17:55:50 +0100

    feat: add images to the grid

--- src/components/index-page/sections/image-gallery/image-gallery.jsx
+++ src/components/index-page/sections/image-gallery/image-gallery.jsx
@@ -5,14 +5,27 @@ import Section from '../../../_ui/base-section/base-section'
 import styles from './image-gallery.module.css'
 import Button from '../../../_ui/button/button'
 
+import image01 from '../../../../images/gallery/001.jpg'
+import image02 from '../../../../images/gallery/002.jpg'
+import image03 from '../../../../images/gallery/003.jpg'
+import image04 from '../../../../images/gallery/004.jpg'
+import image05 from '../../../../images/gallery/005.jpg'
+import image06 from '../../../../images/gallery/006.jpg'
+
 const ImageGallery = () => {
   return (
     <Section>
       <div className={styles.imageGrid}>
-        <div className={styles.imageItem}></div>
-        <div className={styles.imageItem}></div>
-        <div className={styles.imageItem}></div>
-        <div className={cx(styles.imageItem, styles.hightlights)}>
+        <div className={cx(styles.gridItem, styles.imageItem)}>
+          <img src={image01} alt="DevFest 18 photo 01"/>
+        </div>
+        <div className={cx(styles.gridItem, styles.imageItem)}>
+          <img src={image02} alt="DevFest 18 photo 02"/>
+        </div>
+        <div className={cx(styles.gridItem, styles.imageItem)}>
+          <img src={image03} alt="DevFest 18 photo 03"/>
+        </div>
+        <div className={cx(styles.gridItem, styles.hightlights)}>
           <h3>#DevFest18 highlights</h3>
           <p>
             Such a great experience in DevFest Casablanca 18, check these
@@ -20,9 +33,15 @@ const ImageGallery = () => {
           </p>
           <Button color="white">SEE ALL PHOTOS</Button>
         </div>
-        <div className={styles.imageItem}></div>
-        <div className={styles.imageItem}></div>
-        <div className={styles.imageItem}></div>
+        <div className={cx(styles.gridItem, styles.imageItem)}>
+          <img src={image04} alt="DevFest 18 photo 04"/>
+        </div>
+        <div className={cx(styles.gridItem, styles.imageItem)}>
+          <img src={image05} alt="DevFest 18 photo 05"/>
+        </div>
+        <div className={cx(styles.gridItem, styles.imageItem)}>
+          <img src={image06} alt="DevFest 18 photo 06"/>
+        </div>
       </div>
     </Section>
   )
