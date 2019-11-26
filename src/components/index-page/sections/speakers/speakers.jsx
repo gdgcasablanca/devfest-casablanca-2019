7e6b2486b3c4ea80fc23fff5a5440ea919be1bbd
Omar Houmz <omar.houmz@adevinta.com>
2019-11-26 22:45:32 +0100

    fear: add partner phortos

--- src/components/index-page/sections/speakers/speakers.jsx
+++ src/components/index-page/sections/speakers/speakers.jsx
@@ -8,19 +8,21 @@ import azizi from '../../../../images/speakers/azizi.jpg'
 import rahiche from '../../../../images/speakers/rahiche.jpg'
 import baha from '../../../../images/speakers/baha.jpg'
 import ezouhri from '../../../../images/speakers/ezouhri.jpg'
+import abdo from '../../../../images/speakers/abdo.jpg'
 
 const Speakers = () => {
   return (
     <Section classes={styles.speakers} id="Speakers">
       <div className={styles.speakersContent}>
-        <p className="small-text">What is DevFest?</p>
+        <p className="small-text">Meet our</p>
         <h1>Speakers</h1>
-        <p className="p">Some word about speakers</p>
-        <div className={styles.speakersCards}>
-          {speakers.map((item, index) => (
-            <Card {...item} key={index.toString()} />
-          ))}
-        </div>
+        {speakers.length > 0 && (
+          <div className={styles.speakersCards}>
+            {speakers.map((item, index) => (
+              <Card {...item} key={index.toString()} />
+            ))}
+          </div>
+        )}
         {/* <Button>SEE ALL SPEAKERS</Button> */}
       </div>
     </Section>
@@ -31,7 +33,7 @@ const speakers = [
   {
     name: 'Abdelfettah Sghiouar',
     description: 'Cloud Engineer, Google',
-    img: '',
+    img: abdo,
     talkTitle: 'Intro to container orchestration with kubernetes',
   },
   {
--- src/components/index-page/sections/sponsors/sponsors.jsx
+++ src/components/index-page/sections/sponsors/sponsors.jsx
@@ -2,40 +2,37 @@ import React from 'react'
 
 import styles from './sponsors.module.css'
 import Section from '../../../_ui/base-section/base-section'
-import intelLogo from '../../../../images/intel-logo.svg'
 import Button from '../../../_ui/button/button'
 import Link from '../../../_ui/link/link'
+import emsi from '../../../../images/partners/emsi.png'
+import gdg from '../../../../images/logo-gdg.svg'
+import wtm from '../../../../images/logo-wtm.svg'
 
 const sponsors = [
   {
-    name: 'Accenture',
-    img: intelLogo,
+    name: 'Emsi',
+    img: emsi,
   },
   {
-    name: 'Intel',
-    img: intelLogo,
+    name: 'GDG',
+    img: gdg,
   },
   {
-    name: 'Atos',
-    // img: intelLogo,
-  },
-  {
-    name: 'SAP',
-    img: intelLogo,
+    name: 'WTM',
+    img: wtm,
   },
 ]
 
-const Sponsors = () => {
-  return (
-    <Section classes={styles.sponsors} id="Sponsors">
-      <div className={styles.sponsorsContent}>
-        {/* <p className="small-text">---</p> */}
-        <h1>Partners & Sponsors</h1>
-        <p className="p">
-          Speacial thanks to our sponsors for their partnership and support to
-          make the DevFest19 Casablanca experience what it is.
-        </p>
-        <div className={styles.sponsorImages}>
+const Sponsors = () => (
+  <Section classes={styles.sponsors} id="Sponsors">
+    <div className={styles.sponsorsContent}>
+      <h1>Partners & Sponsors</h1>
+      <p className="p">
+        Speacial thanks to our sponsors for their partnership and support to
+        make the DevFest19 Casablanca experience what it is.
+      </p>
+      <div className={styles.sponsorImages}>
+        {sponsors.length > 0 && (
           <div className={styles.sponsorImagesInner}>
             {sponsors.map(({ img, name }, index) =>
               img ? (
@@ -45,18 +42,18 @@ const Sponsors = () => {
               ),
             )}
           </div>
-        </div>
-        <Button
-          As={Link}
-          href="mailto:contact@gdgcasablanca.com"
-          color="white"
-          target="_blank"
-        >
-          BECOME A PARTNER
-        </Button>
+        )}
       </div>
-    </Section>
-  )
-}
+      <Button
+        As={Link}
+        href="mailto:contact@gdgcasablanca.com"
+        color="white"
+        target="_blank"
+      >
+        BECOME A PARTNER
+      </Button>
+    </div>
+  </Section>
+)
 
 export default Sponsors
